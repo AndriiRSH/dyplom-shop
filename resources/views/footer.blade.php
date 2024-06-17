32079442c23dd2ab633f7ba6cee10574a4e8cce2
AndriiRSH <andrii.rsh@gmail.com>
2024-06-17 12:46:20 +0300

    fix

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -16,7 +16,7 @@
         <div class="menu_wrap">
             <a href="https://www.instagram.com/design_moskalenko/" class="fa-brands fa-instagram fa-2xl pl-3"></a>
             <a href="https://www.facebook.com/p/%D0%94%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD-%D1%81%D1%82%D1%83%D0%B4%D0%B8%D1%8F-%D0%A0%D0%BE%D0%BC%D0%B0%D0%BD%D0%B0-%D0%9C%D0%BE%D1%81%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD%D0%BA%D0%BE-100042244844816/" class="fa-brands fa-facebook fa-2xl p-3"></a>
-            <a href="" class="fa-brands fa-telegram fa-2xl"></a>
+{{--            <a href="" class="fa-brands fa-telegram fa-2xl"></a>--}}
         </div>
  </footer>
 
